d6ac06aa6b25a92a97548706088faf7cf93d1a32
Bruno Henrique <bruhensant@gmail.com>
2022-05-01 00:39:40 -0300

    yey

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,53 @@
-console.log('texto');
+var time = 80;
+var downs;
+function lp1_up(ids) {
+    downs = setInterval(function () {
+        var valor = document.getElementById('lp1');
+        var atual = parseInt(valor.innerHTML);
+        var novo = parseInt(valor.innerHTML) + 1;
+        valor.innerHTML = novo.toString();
+        var fonte_atual = getComputedStyle(valor).getPropertyValue('font-weight');
+        var nova_fonte = parseInt(valor.innerHTML) * 10;
+        valor.style.setProperty('font-weight', nova_fonte.toString());
+        console.log(getComputedStyle(valor).getPropertyValue('font-weight'));
+    }, time);
+}
+function lp1_down(ids) {
+    downs = setInterval(function () {
+        var valor = document.getElementById('lp1');
+        var atual = parseInt(valor.innerHTML);
+        var novo = parseInt(valor.innerHTML) - 1;
+        valor.innerHTML = novo.toString();
+        var fonte_atual = getComputedStyle(valor).getPropertyValue('font-weight');
+        var nova_fonte = parseInt(valor.innerHTML) * 10;
+        valor.style.setProperty('font-weight', nova_fonte.toString());
+        console.log(getComputedStyle(valor).getPropertyValue('font-weight'));
+    }, time);
+}
+function lp2_up(ids) {
+    downs = setInterval(function () {
+        var valor = document.getElementById('lp2');
+        var atual = parseInt(valor.innerHTML);
+        var novo = parseInt(valor.innerHTML) + 1;
+        valor.innerHTML = novo.toString();
+        var fonte_atual = getComputedStyle(valor).getPropertyValue('font-weight');
+        var nova_fonte = parseInt(valor.innerHTML) * 10;
+        valor.style.setProperty('font-weight', nova_fonte.toString());
+        console.log(getComputedStyle(valor).getPropertyValue('font-weight'));
+    }, time);
+}
+function lp2_down(ids) {
+    downs = setInterval(function () {
+        var valor = document.getElementById('lp2');
+        var atual = parseInt(valor.innerHTML);
+        var novo = parseInt(valor.innerHTML) - 1;
+        valor.innerHTML = novo.toString();
+        var fonte_atual = getComputedStyle(valor).getPropertyValue('font-weight');
+        var nova_fonte = parseInt(valor.innerHTML) * 10;
+        valor.style.setProperty('font-weight', nova_fonte.toString());
+        console.log(getComputedStyle(valor).getPropertyValue('font-weight'));
+    }, time);
+}
+function stop() {
+    clearInterval(downs);
+}
--- scripts/script.ts
+++ scripts/script.ts
@@ -1,1 +1,76 @@
-console.log('texto');
+
+const time: number = 80;
+
+let downs:any;
+
+function lp1_up(ids:any) {
+    downs = setInterval(() =>{
+    var valor = document.getElementById('lp1');
+    var atual = parseInt(valor.innerHTML);
+    var novo = parseInt(valor.innerHTML) + 1;
+
+    valor.innerHTML = novo.toString();
+
+    var fonte_atual = getComputedStyle(valor).getPropertyValue('font-weight');
+    var nova_fonte = parseInt(valor.innerHTML) * 10;
+    valor.style.setProperty('font-weight',nova_fonte.toString())
+    
+    console.log(getComputedStyle(valor).getPropertyValue('font-weight'));
+
+    },time);
+}
+
+function lp1_down(ids:any) {
+    downs = setInterval(() =>{
+    var valor = document.getElementById('lp1');
+    var atual = parseInt(valor.innerHTML);
+    var novo = parseInt(valor.innerHTML) - 1;
+
+    valor.innerHTML = novo.toString();
+
+    var fonte_atual = getComputedStyle(valor).getPropertyValue('font-weight');
+    var nova_fonte = parseInt(valor.innerHTML) * 10;
+    valor.style.setProperty('font-weight',nova_fonte.toString())
+
+    console.log(getComputedStyle(valor).getPropertyValue('font-weight'));
+    },time);
+}
+
+
+function lp2_up(ids:any) {
+    downs = setInterval(() =>{
+    var valor = document.getElementById('lp2');
+    var atual = parseInt(valor.innerHTML);
+    var novo = parseInt(valor.innerHTML) + 1;
+
+    valor.innerHTML = novo.toString();
+
+    var fonte_atual = getComputedStyle(valor).getPropertyValue('font-weight');
+    var nova_fonte = parseInt(valor.innerHTML) * 10;
+    valor.style.setProperty('font-weight',nova_fonte.toString())
+    
+    console.log(getComputedStyle(valor).getPropertyValue('font-weight'));
+
+    },time);
+}
+
+function lp2_down(ids:any) {
+    downs = setInterval(() =>{
+    var valor = document.getElementById('lp2');
+    var atual = parseInt(valor.innerHTML);
+    var novo = parseInt(valor.innerHTML) - 1;
+    
+    valor.innerHTML = novo.toString();
+
+    var fonte_atual = getComputedStyle(valor).getPropertyValue('font-weight');
+    var nova_fonte = parseInt(valor.innerHTML) * 10;
+    valor.style.setProperty('font-weight',nova_fonte.toString())
+
+    console.log(getComputedStyle(valor).getPropertyValue('font-weight'));
+    },time);
+}
+
+
+function stop(){
+    clearInterval(downs);
+}
