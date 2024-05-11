ed790ed78b75c5105a792706e630f98917498816
Bruno Oliveira <bruhensant@gmail.com>
2024-05-11 20:14:29 -0300

    feat(player): inclui mais cores no dicionário

--- app/4p/page.tsx
+++ app/4p/page.tsx
@@ -1,13 +1,13 @@
 import Player from "../components/player";
 
 const players = [
-	{id: 1, life: 40, color: 'red' },
-	{id: 2, life: 20, color: 'blue' }
+	{id: 1, life: 40, color: 'slate' },
+	{id: 2, life: 20, color: 'yellow' }
 ]
 
 export default function Page() {
   return (
-	<div>
+	<div className="flex flex-col gap-2 p-2">
 		{
 			players.map((player) => 
 				<Player key={player.id} life={player.life} color={player.color}/>
--- app/components/player.tsx
+++ app/components/player.tsx
@@ -1,6 +1,12 @@
 const colorMap: {[key: string]: string} = {
 	red: 'bg-red-500',
 	blue: 'bg-blue-500',
+	green: 'bg-green-500',
+	yellow: 'bg-yellow-500',
+	purple: 'bg-purple-500',
+	pink: 'bg-pink-500',
+	slate: 'bg-slate-500',
+	orange: 'bg-orange-500',
 }
 
 export default function Player({life, color}: {life: number, color:string}) {
